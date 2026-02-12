1e5df71d23011afc7c3068d3f235b89db288417e
khardigan <joseantonio.ferrer.rodriguez.alu@iesfernandoaguilar.es>
2026-02-12 12:42:06 +0100

    funcional1.1

--- proyectoFrontend/src/app/services/auth.ts
+++ proyectoFrontend/src/app/services/auth.ts
@@ -16,3 +16,55 @@ export class AuthInterceptor implements HttpInterceptor {
     return next.handle(authReq);
   }
 }
+
+
+/*
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'http://localhost:8080/auth/login';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(credentials: any) {
+    return this.http.post<any>(this.apiUrl, credentials);
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+
+  getRole(): string | null {
+    const token = this.getToken();
+    if (!token) return null;
+
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    return payload.role; // depende de cómo lo envíes en el backend
+  }
+
+  isAdmin(): boolean {
+    return this.getRole() === 'ROLE_ADMIN';
+  }
+}
+
+
+*/
